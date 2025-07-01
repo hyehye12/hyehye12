06892f3d722d69e6b4322dcfc49e0bcfcf07dd71
hyemin <gtg1349@gmail.com>
2025-07-01 16:13:36 +0900

    0701 오후 수업 commit

--- study/0701/hello.js
+++ study/0701/hello.js
@@ -1,1 +1,22 @@
 console.log("Hello World!!");
+function greet(name) {
+    return "Hello, ".concat(name);
+}
+console.log(greet("Jhon"));
+var n = 1;
+var b = true;
+var s = "Hello";
+var o = {};
+var n1 = 1;
+var b1 = true;
+var s1 = "Hello";
+var o1 = {};
+var a = 0;
+a = "Hello";
+a = true;
+a = {};
+var u = undefined;
+// u = 1; -> 실행안됨
+var count = 10, message = "your count";
+var result = "".concat(message, " is ").concat(count);
+console.log(result);
