c9e10d76f43b16506e31075da785692f406066cd
hyemin <gtg1349@gmail.com>
2025-07-14 11:41:08 +0900

    commit

---
+++ study/class/0714/ch03_4/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DirectionTest from "./pages/DirectionTest";
+
+export default function App() {
+  return (
+    <main>
+      <DirectionTest />
+    </main>
+  );
+}
---
+++ study/class/0714/ch03_4/src/pages/User.tsx
@@ -0,0 +1,11 @@
+import { Title } from "../components";
+
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <Title>CopyMe</Title>
+      <div className="mt-4"></div>
+    </section>
+  );
+}
+// mt-4 는 margin top 4
