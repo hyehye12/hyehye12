961d0ec25e3369d5bdbd19c8eac98fcd17cfec34
hyemin <gtg1349@gmail.com>
2025-08-11 16:48:44 +0900

    Fix: remove accidental nested git repo and add blog as regular folder

---
+++ study/0811/chapter8/blog/src/blog.repository.ts
@@ -0,0 +1,93 @@
+import { readFile, writeFile } from 'fs/promises';
+import { PostDto } from './blog.model';
+import { Injectable } from '@nestjs/common';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Blog, BlogDocument } from './blog.schema';
+
+// 블로그 인터페이스
+export interface BlogRepository {
+  getAllPost(): Promise<PostDto[]>;
+  createPost(postDto: PostDto);
+  getPost(id: String): Promise<PostDto>;
+  deletePost(id: String);
+  updatePost(id: String, postDto: PostDto);
+}
+
+@Injectable()
+export class BlogFileRepository implements BlogRepository {
+  FILE_NAME = './src/blog.data.json';
+
+  async getAllPost(): Promise<PostDto[]> {
+    const datas = await readFile(this.FILE_NAME, 'utf8');
+    const posts = JSON.parse(datas);
+    return posts;
+  }
+
+  async createPost(postDto: PostDto) {
+    const posts = await this.getAllPost();
+    const id = posts.length + 1;
+    const createPost = { id: id.toString(), ...postDto, createdDt: new Date() };
+    posts.push(createPost);
+    await writeFile(this.FILE_NAME, JSON.stringify(posts));
+  }
+
+  async getPost(id: string): Promise<PostDto> {
+    const posts = await this.getAllPost();
+    const result = posts.find((post) => post.id === id);
+    if (!result) {
+      throw new Error(`Post with id ${id} not found`);
+    }
+    return result;
+  }
+
+  async deletePost(id: string) {
+    const posts = await this.getAllPost();
+    const filteredPosts = posts.filter((post) => post.id !== id);
+    await writeFile(this.FILE_NAME, JSON.stringify(filteredPosts));
+  }
+
+  async updatePost(id: string, postDto: PostDto) {
+    const posts = await this.getAllPost();
+    const index = posts.findIndex((post) => post.id === id);
+    const updatePost = { id, ...postDto, updatedDt: new Date() };
+    posts[index] = updatePost;
+    await writeFile(this.FILE_NAME, JSON.stringify(posts));
+  }
+}
+
+@Injectable()
+export class BlogMongoRepository implements BlogRepository {
+  constructor(@InjectModel(Blog.name) private blogModel: Model<BlogDocument>) {}
+
+  async getAllPost(): Promise<Blog[]> {
+    return await this.blogModel.find().exec();
+  }
+
+  async createPost(postDto: PostDto) {
+    const createPost = {
+      ...postDto,
+      createdDt: new Date(),
+      updatedDt: new Date(),
+    };
+    this.blogModel.create(createPost);
+  }
+
+  async getPost(id: string): Promise<PostDto> {
+    const post = await this.blogModel.findById(id);
+    if (!post) {
+      throw new Error(`Post with id ${id} not found`);
+    }
+    return post;
+  }
+
+  async deletePost(id: string) {
+    await this.blogModel.findByIdAndDelete(id);
+  }
+
+  async updatePost(id: string, postDto: PostDto) {
+    const updatePost = { id, ...postDto, updatedDt: new Date() };
+    await this.blogModel.findByIdAndUpdate(id, updatePost);
+  }
+}
---
+++ study/0811/chapter8/blog/src/blog.service.ts
@@ -0,0 +1,30 @@
+import { PostDto } from './blog.model';
+import { BlogMongoRepository } from './blog.repository';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BlogService {
+  constructor(private blogRepository: BlogMongoRepository) {}
+
+  posts = [];
+
+  async getAllPosts() {
+    return await this.blogRepository.getAllPost();
+  }
+
+  createPost(postDto: PostDto) {
+    this.blogRepository.createPost(postDto);
+  }
+
+  async getPost(id): Promise<PostDto> {
+    return await this.blogRepository.getPost(id);
+  }
+
+  delete(id) {
+    this.blogRepository.deletePost(id);
+  }
+
+  updatePost(id, postDto: PostDto) {
+    this.blogRepository.updatePost(id, postDto);
+  }
+}
