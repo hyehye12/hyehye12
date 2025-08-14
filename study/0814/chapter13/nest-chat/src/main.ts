9cca126cd8efa036f6715182b41f20043402459b
hm <jo@naver.com>
2025-08-14 15:38:28 +0900

    수업 commit

---
+++ study/0814/chapter13/nest-chat/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  // NestExoressApplication의 인스턴스 생성
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
