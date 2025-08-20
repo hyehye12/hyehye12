a473d24eb453e73d839c7f827c80032b93639f17
hm <jo@naver.com>
2025-08-20 11:52:06 +0900

    commit

--- study/0814/chapter13/nest-chat/src/app.module.ts
+++ study/0814/chapter13/nest-chat/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatGateway } from './chat/chat.gateway';
+import { ChatGeteway, RoomGatewary } from './app.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  providers: [AppService, ChatGeteway, RoomGatewary],
 })
 export class AppModule {}
--- study/0814/chapter13/nest-chat/src/main.ts
+++ study/0814/chapter13/nest-chat/src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  // NestExoressApplication의 인스턴스 생성
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'static'));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap();
